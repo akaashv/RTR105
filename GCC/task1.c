b65a7c2168608cd61ee526710271d81f462e3878
akaashv <akaashvijayakumar@gmail.com>
2021-10-07 02:09:27 +0300

    taskno1

---
+++ GCC/task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+long long a,b,c; 
+printf("Enter integer 1: \n");
+scanf("%lld",&a);
+printf("Enter integer 2: \n"); 
+scanf("%lld",&b);
+c = a*b; 
+printf("\n Number = %lld \n",c);
+return 0;
+}
