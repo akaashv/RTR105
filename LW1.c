18eb7af60d1f41dc0f779437af1100e27b0f6193
akaashv <akaashvijayakumar@gmail.com>
2021-12-03 03:40:29 +0200

    changes LW1_No1

---
+++ LW1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+double my_arcsin(double x){
+double a,S;
+int k=0;
+a = (1.)*pow(x,2*k+1)/(pow(4,k)*(pow(1.,2))*(2*k+1));
+S=a;
+printf("%.2f\t%8.2f\t%8.2f\n", x,a,S);
+
+while(k<500){
+	k++;
+	a = a*(k*(x*x)*(2*k-1))/(2*(k*k)*(2*k+1));
+	S = S + a;
+	printf("%.2f\t%8.2f\t%8.2f\n", x,a,S);
+	}
+return S;
+}
+
+void main(){
+	double x=0.5, y, yy;
+	y= asin(x);
+	printf("standard function arcsin - y= arcsin(%.2f)=%.2f\n",x,y);
+	yy= my_arcsin(x);
+	printf("user function - y= my_arcsin(%.2f)=%.2f\n",x,yy);
+}
+
