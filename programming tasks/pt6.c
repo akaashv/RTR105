dc85d5f877712beb3ceaf02ba9ee0e56ae87c495
akaashv <89970879+akaashv@users.noreply.github.com>
2022-01-05 03:49:27 +0200

    Update pt6.c

--- programming tasks/pt6.c
+++ programming tasks/pt6.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
-int main()
+//all in one calculator
+void sum();// no argument no return
+float sub(void); //no argument with return
+void mult(float, float); //With Argument no Return Type
+float divi(float, float); //With Argument and with Return Type
+void main()
 {
-    printf("f1() f2() f3() f4() are functions \n");
-    void f1(void){
-        printf("no argument, no return \n");
-    }
-     
-    void f2(int a){
-        printf("with argument, no Return \n");
-    }
+    // no argument no return
+    sum();
+    //no argument with return
+    float s;
+   s=sub();
+   printf("subtracted value:%f",s);
+   //With Argument Without Return Type
+    float x, y;
+    mult(x, y);
+    //With Argument and with Return Type
+   float u,v,d;
+   d = divi(u,v);
+   printf("quotient value:%f",d);
+}
+
+void sum()  // no argument no return
+{
+    float a, b, sum=0;
+    printf("enter a and b to add:");
+    scanf("%f%f", &a, &b);
+    sum =a+b;
+    printf("sum:%f",sum);
+}
+
+
+float sub(void) //no argument with return
+{
+    float c, d, sub=0;
+    printf("enter c and d to subtract:");
+    scanf("%f%f", &c, &d);
+    sub =c-d;
+    return sub;
     
-    int f3(void){
-     printf("no argument, with Return \n");
-        return 1;
-   }
-    
-    int f4(int a){
-        printf("with argument, with Return \n");
-        return 1;
-    }
-    return 0;
+}
+
+
+void mult(float x, float y)
+{
+    float mult=0;
+    printf("enter x and y to multiply:");
+    scanf("%f%f", &x, &y);
+    mult =x*y;
+    printf("multiple:%f",mult);
+}
+
+float divi(float u, float v) //With Argument and with Return Type
+{
+    float divi=0;
+    printf("enter u and v where u is divided by v:");
+    scanf("%f%f", &u, &v);
+    divi =u/v;
+    return divi;
 }
