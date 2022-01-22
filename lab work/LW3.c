c4f174cd2c8e67225cc29bfc995fbb32ae952a1a
akaashv <89970879+akaashv@users.noreply.github.com>
2022-01-22 10:43:00 +0200

    Add files via upload

--- lab work/LW3.c
+++ lab work/LW3.c
@@ -1,19 +1,22 @@
-
 //https://www.youtube.com/watch?v=zNPNntM_hNM
 #include <stdio.h>
 #include<math.h>
 
 
 void main(){
+FILE * fpointer;
+fpointer = fopen("derivative.dat","w");
+
     float a, b, x, delta_x, fdiff1, diff1,fdiff2,diff2;
     //float y;
-    printf("enter value of a:");
+    printf("note: -1<=x<=1\n");
+    printf("enter value of a:\n");
     scanf("%f",&a);
-    printf("enter value of b:");
+    printf("enter value of b:\n");
     scanf("%f",&b);
-    printf("enter value of precision:");
+    printf("enter value of precision:\n");
     scanf("%f",&delta_x);
-    printf("\tx\t\tsin(x)\t\tsin\'(x)\t\tsin\'(x) analytical\t   sin\''(x)\tsin\''(x) analytical\n");
+   fprintf(fpointer,"\tx\t\tasin(x)\t\t\tasin\'(x)\t\tasin\'(x) analytical\t   asin\''(x)\tasin\''(x) analytical\n");
     x=a;
     while(x<b){
         // y=sin(x);
@@ -22,10 +25,12 @@ void main(){
          diff2 = x/sqrt(pow(1-pow(x,2),3));
         fdiff2= (asin(x+delta_x)-(2*asin(x))+asin(x-delta_x))/pow(delta_x,2);
       //printf("%10.2f\t%10.2f\t%10.2f\t%10.2f\n",x,sin(x), (sin(x+delta_x)-sin(x))/delta_x,cos(x));
-      printf("%10.2f\t %10.2f\t %10.2f\t\t %10.2f\t\t %10.2f\t %10.2f\n\n", x,asin(x), fdiff1,diff1,fdiff2,diff2);
+     fprintf(fpointer,"%10.2f\t %10.2f\t\t %10.2f\t\t\t %10.2f\t\t %10.2f\t %10.2f\n\n", x,asin(x), fdiff1,diff1,fdiff2,diff2);
        //imp printf("%10.2f\t\t%10.2f\t\t%10.2f\t\t%10.2f\t\t%10.2f\t\t%10.2f\n",x,asin(x), fdiff1,diff1,fdiff2,diff2);
        // printf("%10.2f\t%10.2f\n",x,y);
         x +=delta_x;// x= x+ delta_x;
     }
-    
+//FILE * fpointer;
+//fpointer = fopen("derivative.txt","w");
+fclose(fpointer);    
 }
